9ac736ecbfa3e177decefba9bd27459a71f4d3cf
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-08-12 13:09:39 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#/usr/bin/python3
+
 import argparse
 from umsktjson import *
 
 def process_options():
-    parser = argparse.ArgumentParser(description="umskt ")
-    parser.add_argument("-b", "--binkid", type=str, help="specify which BINK identifier to load (defaults to 2E)")
-    parser.add_argument("-c", "--channelid", type=int, help="specify which Channel Identifier to use (defaults to 640)")
-    parser.add_argument("-f", "--file", type=str, help="specify which keys file to load")
+    nothing = ""
+    parser = argparse.ArgumentParser(description="umskt ", usage=nothing)
+    parser.add_argument("-b", "--binkid", metavar=nothing, type=str, help="specify which BINK identifier to load (defaults to 2E)")
+    parser.add_argument("-c", "--channelid", metavar=nothing, type=int, help="specify which Channel Identifier to use (defaults to 640)")
+    parser.add_argument("-f", "--file", metavar=nothing, type=str, help="specify which keys file to load")
 
     args = parser.parse_args()
     return args
@@ -16,6 +19,9 @@ def main():
     if args.file is None:
             args.file = "keys.json"
             
+    if args.binkid is None:
+    	args.binkid = "2E"
+    
     if args is not None:
         if args.channelid is None:
             args.channelid = 640
@@ -27,3 +33,4 @@ def main():
 if __name__ == "__main__":
     main()
 
+.
