2c4ec6338746d464cef2ceaa3fd6bcda5f5315e7
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-08-23 16:56:33 +1200

    Add mod7

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import argparse
 from umsktjson import *
+from umsktjson.mod7 import KeyGenerator
 
 nothing = ""
 def process_options():
     parser = argparse.ArgumentParser(description="usage: umskt ", usage=argparse.SUPPRESS)
-    parser.add_argument("-b", "--binkid", metavar=nothing, type=str, help="Bink ID option")
-    parser.add_argument("-c", "--channelid", metavar=nothing, type=int, help="Channel ID option")
-    parser.add_argument("-f", "--file", metavar=nothing, type=str, help="File option")
-    parser.add_argument("-V", "--verify", metavar=nothing, type=str, help="product key to validate signature")
+    parser.add_argument("-b","--binkid", metavar=nothing, type=str, help="Bink ID option")
+    parser.add_argument("-c","--channelid", metavar=nothing, type=int, help="Channel ID option")
+    parser.add_argument("-f","--file", metavar=nothing, type=str, help="File option")
+    parser.add_argument("-V","--verify", metavar=nothing, type=str, help="product key to validate signature")
     parser.add_argument("-v","--verbose",action="store_true",help="enable verbose output")
     parser.add_argument("-l","--list",action="store_true",help="show which products/binks can be loaded")
+    parser.add_argument("-m", "--mod7", help="Generate pkeys that use the mod7 algorithm followed by extra keywords")
 
     args = parser.parse_args()
     return args
@@ -20,7 +22,7 @@ def main():
     if args.list is None:
         args.list = False
         
-    if args is not None and args.verify is None and args.list == False:
+    if args is not None and args.verify is None and args.list == False and args.mod7 is None:
         if args.channelid is None:
             args.channelid = 640
         if args.file is None:
@@ -46,7 +48,15 @@ def main():
         
     if args is not None and args.list == True:
         listkeys()
-        
+    
+    if args.mod7:
+        if not args.mod7:
+           pass
+        if args.mod7 == "retail":
+            print(KeyGenerator.retailkey())
+        if args.mod7 == "oem":
+            print(KeyGenerator.oemkey())
+          
     if args is None:
         key = generate_key()
         if len(key) == 24:
